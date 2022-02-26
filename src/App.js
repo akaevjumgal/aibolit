05b31a5e130c1781ecce3cdf7f3f6d8643c91649
fAyday-everyday <polkiju489@gmaill.com>
2022-02-26 20:42:56 +0600

    icons added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import DirectionCard from './components/card/Card';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Aibolit</h1>
+      <DirectionCard/>
     </div>
   );
 }
