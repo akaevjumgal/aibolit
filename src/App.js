1f82eebbcd7e3adff22767a2e2d929daa188e7b0
zhumgal.akaev.kg <jumgal.akaev@zensoft.io>
2022-02-26 20:22:47 +0600

    initialized basic working structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Aibolit</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
