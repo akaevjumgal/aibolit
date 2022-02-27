bb8fc18a5f16c0298865fa360463ead63347b8af
Kasymov Marsel <marselkasymov23@gmail.com>
2022-02-27 18:28:41 +0600

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
-import './Footer.css'
+import './Footer.css';
+import { Link } from "react-router-dom";
 
 export default function Footer(){
 
@@ -17,63 +18,76 @@ export default function Footer(){
                     <h5>ветеринарная клиника</h5>
 
                 </div>
+            <div className="footer__contacts">
+                <div className="footer__download">
+                    <Link to='#'><img src={'/img/appStore.svg'} alt="" /></Link>
+                    <Link to='#'><img src={'/img/google-play.svg'} alt="" /></Link>
+                    
+                </div>
+                <div className="footer__social">
+                    <Link to='#'><img src={'/img/inst.svg'} alt="" /></Link>
+                    <Link to='#'><img src={'/img/fb.svg'} alt="" /></Link>
+                    <Link to='#'><img src={'/img/vk.svg'} alt="" /></Link>
+                    <Link to='#'><img src={'/img/youtube.svg'} alt="" /></Link>
+                </div>
+                <div className="footer__mail">
+                    <p>aibolit34.ru</p>
+                    <p>aibolit34@gmail.com</p>
+                </div>
+            </div>
             </div>
             <div className="footer__description">
                  <div className='confidational'>
                      <p>Политика <br/> конфиденциальности в отношении обработки персональных данных</p>
                  </div>
                  <div className='footer__list'>
-                     <ul>
-                         <li>
-                         <a href="">Главная</a>
+                     <ul className='footer__list__1'>
+                     <li>
+                         <Link to='#' className='footer__info'>Главная</Link>
                          </li>
                          <li>
-                         <a href="">Услуги и цены</a>
+                         <Link to='#' className='footer__info'>Услуги и цены</Link>
                          </li>
                          <li>
-                         <a href="">История компании</a>
+                         <Link to='#' className='footer__info'>История компании</Link>
                          </li>
                          <li>
-                         <a href="">Наши специалисты</a>
+                         <Link to='#' className='footer__info'>Наши специалисты</Link>
                          </li>
                          <li>
-                         <a href="">Вакансии</a>
+                         <Link to='#' className='footer__info'>Вакансии</Link>
                          </li>
                          <li>
-                         <a href="">Реквизиты</a>
+                         <Link to='#' className='footer__info'>Реквизиты</Link>
                          </li>
                          <li>
-                         <a href="">Онлайн-оплата</a>
+                         <Link to='#' className='footer__info'>Онлайн-оплата</Link>
                          </li>
                      </ul>
-                     <ul>
-                     <li>
-                         <a href="">Главная</a>
+                    <ul className='footer__list__2'>
+                         
+                         <li>
+                         <Link to='#' className='footer__info'>Отзывы</Link>
                          </li>
                          <li>
-                         <a href="">Услуги и цены</a>
+                         <Link to='#' className='footer__info'>Контакты</Link>
                          </li>
                          <li>
-                         <a href="">История компании</a>
+                         <Link to='#' className='footer__info'>Груминг-салон</Link>
                          </li>
                          <li>
-                         <a href="">Наши специалисты</a>
+                         <Link to='#' className='footer__info'>Косметика</Link>
                          </li>
                          <li>
-                         <a href="">Вакансии</a>
+                         <Link to='#' className='footer__info'>Новости и акции</Link>
                          </li>
                          <li>
-                         <a href="">Реквизиты</a>
-                         </li>
-                         <li>
-                         <a href="">Онлайн-оплата</a>
+                         <Link to='#' className='footer__info'>Блог</Link>
                          </li>
                      </ul>
                  </div>
-                 
-                    
-                 
             </div>
+            
         </div>
     )
 }
