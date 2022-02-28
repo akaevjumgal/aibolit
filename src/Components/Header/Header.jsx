e2f2283d808930dcd5e8dc442621101db048b809
Kasymov Marsel <marselkasymov23@gmail.com>
2022-02-28 11:11:08 +0600

    hover

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import './Header.css';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 export default function Header(){
-
-
+    const [active,setActive] = useState('header__info')
+    function toggle(){
+        if (active === 'header__info' ) {
+            setActive('active');
+            
+        }   
+        else {
+            setActive ('header__info')
+        }
+    }
+    
 
     return(
         <div className="header__container">
@@ -11,11 +21,11 @@ export default function Header(){
                 <Link to='#' className="logo"><img src={'/img/Logo.svg'} alt="Логотип" /></Link>
             </div>
             <div className="header__info__container">
-               <Link to='#' className='header__info'> Личный кабинет</Link>
-               <Link to='#' className='header__info'> О нас</Link>
-               <Link to='#' className='header__info'>Новости и акции</Link>
-               <Link to='#' className='header__info'> Контакты</Link>
-               <Link to='#' className='header__info'> Блог</Link>
+               <Link to='#' onClick={() => toggle()} className={active}> Личный кабинет</Link>
+               <Link to='#' onClick={() => toggle()} className={active }> О нас</Link>
+               <Link to='#' onClick={() => toggle()} className={active}>Новости и акции</Link>
+               <Link to='#' onClick={() => toggle()} className={active}> Контакты</Link>
+               <Link to='#' onClick={() => toggle()} className={active}> Блог</Link>
             </div>
             
                 <div className='city__info'>
