5ba4c2ae5fc6e89d24e2ab89d09c8046fb507ed4
Islam52 <islamhihiza11@gmail.com>
2022-03-15 18:37:29 +0600

    Maps

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import SectionOne from './../../modules/HomePage/section-one/SectionOne';
+import Maps from './../../modules/HomePage/Maps/Maps';
 
 export default function HomePage() {
   return (
     <>
       <SectionOne />
+      <Maps />
     </>
     
   )
