3cbf8eddfcb63e8851e46bf8206f5fed4444ef58
Nursultan34 <nursultanerikov7@gmail.com>
2022-03-19 19:36:38 +0600

    modal

--- src/modules/OTPVerification/OTPVerificationModule.jsx
+++ src/modules/OTPVerification/OTPVerificationModule.jsx
@@ -1,19 +1,26 @@
-import { ModalScenes } from "../../common/signin";
+import { useNavigate } from 'react-router-dom';
+import { ModalScenes } from '../../common/signin';
 import CustomButton from '../../components/Button/Button';
 import CountDown from '../../components/CountTimer/Timer';
-import VerificationInput from "../../components/VerificationInput/VerificationInput";
+import VerificationInput from '../../components/VerificationInput/VerificationInput';
 import styles from './OTPVerificationModule.module.css';
 
 export default function OTPVerificationModule({ setScene }) {
-    return (
-        <div>
-            <h1 align="center" className={styles.h2}>Вход или регистрация</h1>
-            <VerificationInput />
-            <CountDown />
-            <div className={styles.buttonWrapper}>
-                <CustomButton>Готово</CustomButton>
-            </div>
-            <p className={styles.text} onClick={() => setScene(ModalScenes.PhoneNumber)}>Назад</p> 
-        </div>
-    )
+  const navigade = useNavigate();
+
+  const openProfile = () => {
+    navigade('/PersonalAccount', { replace: true });
+  };
+
+  return (
+    <div>
+      <h1 align="center" className={styles.h2}>Вход или регистрация</h1>
+      <VerificationInput />
+      <CountDown />
+      <div className={styles.buttonWrapper}>
+        <CustomButton  onClick={openProfile}>Готово</CustomButton>
+      </div>
+      <p className={styles.text} onClick={() => setScene(ModalScenes.PhoneNumber)}>Назад</p>
+    </div>
+  );
 }
