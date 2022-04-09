a794dce7c6002af4e6f63de0aea0405787feadab
zhumgal.akaev.kg <jumgal.akaev@zensoft.io>
2022-04-09 19:49:21 +0600

    renamed icon

--- src/pages/HistoryPage/HistoryPage.jsx
+++ src/pages/HistoryPage/HistoryPage.jsx
@@ -1,33 +1,32 @@
-import { useContext, useEffect } from "react";
-import NavigationContext from "../../navigation.context";
-import { Link } from "react-router-dom";
-import styles from '../HistoryPage/HistoryPage.module.css'
-import HistoryCard from "../../modules/HistoryCard/HistoryCard";
+import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import NavigationContext from '../../navigation.context';
+import styles from './HistoryPage.module.css';
+import HistoryCard from '../../modules/HistoryCard/HistoryCard';
 
-export default function HistoryPage(){
-  
-  const { setNavigation } = useContext(NavigationContext)
+export default function HistoryPage() {
+  const { setNavigation } = useContext(NavigationContext);
 
   useEffect(() => {
     setNavigation({
       title: 'История компании',
       links: [
-      <a href="#subtitle_one">ПОЗДАГОЛОВОК 1</a> ,
-      <a href="#subtitle_two">ПОЗДАГОЛОВОК 2</a> , 
-      <a href="#subtitle_three">ПОЗДАГОЛОВОК 3</a>
-      ]
-    })
-  },[])
+        <a href="#subtitle_one">ПОЗДАГОЛОВОК 1</a>,
+        <a href="#subtitle_two">ПОЗДАГОЛОВОК 2</a>,
+        <a href="#subtitle_three">ПОЗДАГОЛОВОК 3</a>,
+      ],
+    });
+  }, []);
 
   return (
     <section>
       <div>
-        <Link to='/' className={styles.back}>
-          <img src="/img/HistoryPage/path<.png" alt="<" className={styles.back_btn}/>
+        <Link to="/" className={styles.back}>
+          <img src="/img/HistoryPage/path.png" alt="<" className={styles.back_btn} />
           назад
         </Link>
       </div>
-      <HistoryCard/>
+      <HistoryCard />
     </section>
-  )
+  );
 }
